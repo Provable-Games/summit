b037590ae3cd36c853f523558922892439b264cb
loothero <loothero@protonmail.com>
2024-08-29 20:04:21 +0000

    add starkli with tooling for sepolia/mainnet deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,10 +10,10 @@ slot deployments delete savagesummit torii
 slot deployments delete savagesummit katana
 
 # echo "Deploying world to Realms L3..."
-slot deployments create savagesummit katana --version v1.0.0-alpha.6 --disable-fee true --block-time 1000 --accounts 10
+slot deployments create savagesummit katana --version v1.0.0-alpha.8 --disable-fee true --block-time 1000 --accounts 10
 
 # echo "Migrating world..."
 sozo --release migrate apply
 
 # echo "Setting up remote indexer on slot..."
-slot deployments create savagesummit torii --version v1.0.0-alpha.6 --world 0x8d8e73b20e205f98347501a072701e3b7b55c6048ff78562c025aa8a5571a0 --rpc https://api.cartridge.gg/x/savagesummit/katana/ --start-block 0 --index-pending true
+slot deployments create savagesummit torii --version v1.0.0-alpha.8 --world 0x8d8e73b20e205f98347501a072701e3b7b55c6048ff78562c025aa8a5571a0 --rpc https://api.cartridge.gg/x/savagesummit/katana/ --start-block 0 --index-pending true
