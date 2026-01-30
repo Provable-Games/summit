355e186028a49606d3be5d7ab2d65b0fa0582c55
Await-0x <146035814+Await-0x@users.noreply.github.com>
2026-01-30 13:33:58 +0100

    set test_money batch to 300

--- client/src/components/dialogs/ClaimTestMoney.tsx
+++ client/src/components/dialogs/ClaimTestMoney.tsx
@@ -5,7 +5,7 @@ import { gameColors } from '@/utils/themes';
 import { Box, Button, Dialog, Typography } from '@mui/material';
 import { useEffect, useMemo, useState } from 'react';
 
-const LIMIT = 500;
+const LIMIT = 300;
 
 function ClaimTestMoney(props) {
   const { open, close } = props;
