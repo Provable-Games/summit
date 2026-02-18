100afeae8e38d43c670f80c0a245f71acc1bcd58
Await-0x <146035814+Await-0x@users.noreply.github.com>
2026-02-18 13:07:38 +0100

    deploy release

--- indexer/apibara.config.ts
+++ indexer/apibara.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   runtimeConfig: {
     summit: {
       // Summit game contract address (mainnet)
-      summitContractAddress: "0x00cd57105c502a7d5e477620b5544766a8a77cb5d0b2ccdc6983fcd0027f47c5",
+      summitContractAddress: "0x0214d382e80781f8c1059a751563d6b46e717c652bb670bf230e8a64a68e6064",
       // Beasts NFT contract address (mainnet)
       beastsContractAddress: "0x046da8955829adf2bda310099a0063451923f02e648cf25a1203aac6335cf0e4",
       // Dojo World contract address (Loot Survivor mainnet)
@@ -14,9 +14,9 @@ export default defineConfig({
       // CollectableEntity dungeon filter (Loot Survivor dungeon)
       collectableEntityDungeon: "0x00a67ef20b61a9846e1c82b411175e6ab167ea9f8632bd6c2091823c3629ec42",
       // Corpse contract address (mainnet)
-      corpseContractAddress: "0x01f40a78e8d3e0687e30fc173a28cc62cdf976187f23f778b792a71f16e4482f",
+      corpseContractAddress: "0x0103eafe79f8631932530cc687dfcdeb013c883a82619ebf81be393e2953a87a",
       // Skull contract address (mainnet)
-      skullContractAddress: "0x05dff27b8cdef20e537b5a33bf1feb4dbc5fb0ebfcb59e33cd95a075f5eb8916",
+      skullContractAddress: "0x01c3c8284d7eed443b42f47e764032a56eaf50a9079d67993b633930e3689814",
       // Mainnet DNA stream URL
       streamUrl: process.env.STREAM_URL,
       // Starting block - use earliest block needed for Dojo events
